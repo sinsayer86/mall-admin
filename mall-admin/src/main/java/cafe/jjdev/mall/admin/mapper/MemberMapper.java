16852e69360e19141ac4b9724655f4ea4c03e900
Administrator <Administrator@MSDN-SPECIAL>
2019-05-15 17:30:33 +0900

    1

--- mall-admin/src/main/java/cafe/jjdev/mall/admin/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package cafe.jjdev.mall.admin.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import cafe.jjdev.mall.admin.vo.Member;
-
-@Mapper
-public interface MemberMapper {
-	public Member selectMember(Member member);
-}
